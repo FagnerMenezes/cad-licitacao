8702b764c80f077b838e6ccb323dc88c1c2e1627
Fagner Menezes <fsantosmenezes@gmail.com>
2023-08-11 16:55:38 -0300

    alter  main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ env.config();
 const app = express();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use("/", express.static(fileURLToPath(__dirname, "./build")));
+app.use("/", express.static(fileURLToPath(__dirname, "./dist")));
 const port = env.PORT_FRONTEND || 3000;
 
 app.listen(port, (err) => {
