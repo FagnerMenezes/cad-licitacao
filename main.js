2b2ae9cfe5e9272a3938a163a05fb745c87a79fe
Fagner Menezes <fsantosmenezes@gmail.com>
2023-08-11 16:51:24 -0300

    add main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { dirname } from "path";
+import env from "react-dotenv";
+import { fileURLToPath } from "url";
+env.config();
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use("/", express.static(fileURLToPath(__dirname, "./build")));
+const port = env.PORT_FRONTEND || 3000;
+
+app.listen(port, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Servidor rodando na porta: " + port);
+});
