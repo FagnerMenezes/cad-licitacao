3a8ce04746a4214e19571c2494218ee2ca25b527
Fagner Menezes <fsantosmenezes@gmail.com>
2023-09-05 18:04:24 -0300

    update services api

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  //baseURL: "http://www.cadlicita.kinghost.net:21052/",
+  baseURL: "http://www.cadlicita.kinghost.net:21052/",
   //baseURL: "https://api-licitacao.vercel.app/",
-  baseURL: "http://localhost:21052/",
+  //baseURL: "http://localhost:21052/",
   withCredentials: false,
 });
